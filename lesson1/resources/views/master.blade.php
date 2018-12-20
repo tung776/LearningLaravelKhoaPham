719d76a8847595ca040ce490f3bdd4c660d53309
nguyễn Thanh Tùng <thanhtung776@gmail.com>
2018-12-20 22:18:42 +0700

    step 1 render view master without layout
    step 1 render view master without layout

---
+++ lesson1/resources/views/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sơn Cát Tường</title>
+    <style>
+        #wrapper {
+            width: 980px;
+            height: auto;
+            margin: 0 auto;
+        }
+        #header {
+            width: auto;
+            height: 200px;
+            background-color: red;
+        }
+        #content {
+            width: auto;
+            height: 500px;
+            background-color: blue;
+        }
+        #footer {
+            width: auto;
+            height: 200px;
+            background-color: greenyellow;
+        }
+    </style>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">header</div>
+        <div id="content">content</div>
+        <div id="footer">footer</div>
+    </div>
+</body>
+</html>
