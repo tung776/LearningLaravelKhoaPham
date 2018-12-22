ddce87f7508b74e190f51666ec5ebc8e55b26ec5
nguyen thanh tung <thanhtung776@gmail.com>
2018-12-22 09:20:33 +0700

    blade template while, forearch, for

--- lesson1/resources/views/footer.blade.php
+++ lesson1/resources/views/footer.blade.php
@@ -1,4 +1,19 @@
+<?php $count =10 ?>
 @extends('layout')
-@section('footer-content')
-    <h1>content footer</h1>
+@section ('content')
+    <?php $y=0 ?>
+    @while ($y< $count)
+        <p>vong while {{$y}}</p>
+        <?php $y++ ?>
+    @endwhile
+    <br/>
+    <?php $arr=['mang 1', 'mang 2', 'mang 3'] ?>
+    @foreach ($arr as $item)
+        <p>item in array: {{ $item }}</p>
+    @endforeach
+@endsection
+@section('footer-content')    
+    @for ($i = 0; $i < $count; $i++)
+        <p>vòng for {{ $i}}</p>        
+    @endfor
 @endsection
