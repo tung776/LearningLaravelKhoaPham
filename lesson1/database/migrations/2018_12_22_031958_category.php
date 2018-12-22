5a7436a65711146170c6176332108e6b016e42c0
tung776 <thanhtung776@gmail.com>
2018-12-22 10:51:00 +0700

    init lession project

--- lesson1/database/migrations/2018_12_22_031958_category.php
+++ lesson1/database/migrations/2018_12_22_031958_category.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProduct extends Migration
+class Category extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateProduct extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->float('price')->nullable()->default(0);
-            $table->integer('quantity')->default(0);
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
---
+++ lesson1/database/migrations/2018_12_22_032024_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Product extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foregin('category_id')
+                    ->references('id')->on('category');
+            $table->string('name');
+            $table->float('price')->nullable()->default(0);
+            $table->integer('quantity')->default(0);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
